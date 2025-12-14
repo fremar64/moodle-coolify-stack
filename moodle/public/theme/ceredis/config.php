f0313aef2e72603b5386d608c85ada6799440634
Frédéric OUAMBA <frederic.ouamba@ceredis.net>
2025-12-14 15:38:17 +0100

    modification du fichier config.php du thème enfant ceredis

--- moodle/public/theme/ceredis/config.php
+++ moodle/public/theme/ceredis/config.php
@@ -19,13 +19,99 @@ $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
 // Feuilles de style personnalisées
 $THEME->sheets = ['ceredis'];
 
-// Régions de blocs
+// Régions de blocs - TOUS LES LAYOUTS NÉCESSAIRES
 $THEME->layouts = [
+    // La page d'accueil du site (notre layout personnalisé)
     'frontpage' => [
         'file' => 'frontpage.php',
         'regions' => ['side-pre'],
         'defaultregion' => 'side-pre',
         'options' => ['nonavbar' => false, 'langmenu' => true],
+    ],
+    // Layouts hérités de Boost (obligatoires)
+    'base' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'standard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'course' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['langmenu' => true],
+    ],
+    'coursecategory' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'incourse' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'admin' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'mydashboard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['nonavbar' => true, 'langmenu' => true],
+    ],
+    'mypublic' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'login' => [
+        'file' => 'login.php',
+        'regions' => [],
+        'options' => ['langmenu' => true, 'nonavbar' => true],
+    ],
+    'popup' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => true],
+    ],
+    'frametop' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => false],
+    ],
+    'embedded' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+    ],
+    'maintenance' => [
+        'file' => 'maintenance.php',
+        'regions' => [],
+    ],
+    'print' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => false],
+    ],
+    'redirect' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+    ],
+    'report' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'secure' => [
+        'file' => 'secure.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
     ],
 ];
 
