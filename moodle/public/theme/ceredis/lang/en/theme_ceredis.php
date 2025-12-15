c677b6e0b49b12aa0ed22c0273d292e0cb6313d2
Frédéric OUAMBA <frederic.ouamba@ceredis.net>
2025-12-15 04:30:44 +0100

    fix: Ajouter le fichier de langue anglais manquant pour le thème CEREDIS
    - Création du fichier lang/en/theme_ceredis.php (requis par Moodle)
    - Résout l'erreur [pluginname,theme_ceredis] lors de la mise à jour
    - Moodle exige toujours un fichier de langue en (anglais) par défaut

---
+++ moodle/public/theme/ceredis/lang/en/theme_ceredis.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'CEREDIS';
+$string['choosereadme'] = 'Theme for CEREDIS Online School';
+$string['configtitle'] = 'CEREDIS';
+
+// Hero section
+$string['platform_title'] = 'Online School';
+$string['platform_tagline'] = 'Adaptive digital learning environment';
+
+// Buttons
+$string['my_courses'] = 'My Courses';
+$string['manage_courses'] = 'Manage Courses';
+$string['start_learning'] = 'Start Learning';
+$string['view_progress'] = 'View Progress';
+$string['help'] = 'Help';
+
+// Messages
+$string['guest_message'] = 'Welcome! Log in to access your personalized learning space.';
+$string['welcome_title'] = 'Welcome to your learning space!';
+$string['welcome_text'] = 'Discover interactive and fun activities in French, mathematics, science and more. Progress at your own pace through personalized learning paths and become a digital learning champion!';
+
+// Categories (4 levels)
+$string['category_rally'] = 'School Rally';
+$string['category_rally_desc'] = 'Educational challenges and competitions for all levels';
+$string['access_rally'] = 'Access Rally';
+
+$string['category_preschool'] = 'Preschool';
+$string['category_preschool_desc'] = 'Playful learning for toddlers (3-6 years)';
+$string['access_preschool'] = 'Access Preschool';
+
+$string['category_primary'] = 'Primary School';
+$string['category_primary_desc'] = 'Grades 1-5: reading, math, discovery';
+$string['access_primary'] = 'Access Primary';
+
+$string['category_secondary'] = 'Middle School';
+$string['category_secondary_desc'] = 'Grades 6-9: deepening and excellence';
+$string['access_secondary'] = 'Access Middle School';
+
+// Subjects
+$string['subject_french'] = 'French';
+$string['subject_math'] = 'Mathematics';
+$string['subject_science'] = 'Science';
+$string['subject_arts'] = 'Arts';
+$string['subject_pe'] = 'Physical Education';
+$string['subject_english'] = 'English';
+$string['subject_history'] = 'History-Geography';
+$string['subject_tech'] = 'Technology';
+$string['subject_languages'] = 'Languages';
+
+// Features
+$string['feature_gamification'] = 'Learning through fun';
+$string['feature_gamification_desc'] = 'Gamified activities that make learning exciting';
+$string['feature_progress'] = 'Progress tracking';
+$string['feature_progress_desc'] = 'Visualize your evolution and achievements in real time';
+$string['feature_support'] = 'Personalized support';
+$string['feature_support_desc'] = 'Your teachers follow and help you in your learning journey';
+$string['feature_rewards'] = 'Rewards and badges';
+$string['feature_rewards_desc'] = 'Collect badges and unlock rewards';
+
+// Sponsors
+$string['sponsors_title'] = 'Our Partners and Sponsors';
+
+// Footer
+$string['footer_about'] = 'About';
+$string['footer_about_text'] = 'CEREDIS offers an adaptive digital learning environment from preschool to middle school, based on competencies and differentiated pedagogy.';
+$string['footer_links'] = 'Quick Links';
+$string['footer_courses'] = 'All Courses';
+$string['footer_calendar'] = 'Calendar';
+$string['footer_badges'] = 'Badges';
+$string['footer_messages'] = 'Messages';
+$string['footer_support'] = 'Support';
+$string['footer_help'] = 'Help';
+$string['footer_contact'] = 'Contact';
+$string['footer_faq'] = 'FAQ';
+$string['footer_privacy'] = 'Privacy';
+$string['footer_contact_us'] = 'Contact Us';
+$string['footer_tagline'] = 'Education in the digital age';
+$string['footer_developed'] = 'Developed with ❤️ for children\'s education';
+
+// Region names
+$string['region-side-pre'] = 'Right';
